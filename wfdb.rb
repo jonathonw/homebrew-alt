bbd553aafdbe567371c83545242be44d31fdf422
Jonathon Williams <gandalf72487@yahoo.com>
2015-01-05 18:03:02 -0600

    Update WFDB formula for 10.5.23

--- wfdb.rb
+++ wfdb.rb
@@ -1,15 +1,12 @@
 # Archives: http://www.physionet.org/physiotools/archives/wfdb-10.5/
 class Wfdb < Formula
   homepage 'http://www.physionet.org/physiotools/wfdb.shtml'
-  url 'http://www.physionet.org/physiotools/archives/wfdb-10.5.13.tar.gz'
-  md5 'e08da98ae61400efced69f01529b8c0a'
+  url 'http://www.physionet.org/physiotools/archives/wfdb-10.5.23.tar.gz'
+  sha256 '755955227caca075296d691847c25f3b6c5354ed2155f54706eb182e39d7f5be'
 
   def install
-    inreplace "conf/darwin.def", "-arch ppc", ""
-    inreplace "conf/darwin-slib.def", "-arch ppc", ""
-
-    system "./configure", "", "--prefix=#{prefix}", "-m32", "--mandir=#{man}"
-    system "make install"
+    system "./configure"
+    system "make", "install", "WFDBROOT=${prefix}"
   end
 
   def test
